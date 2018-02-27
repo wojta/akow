2754e32df56882dc4bbb54a5a741c2f691693765
Vojtěch Sázel <vojtech@sazel.cz>
2018-02-27 16:03:15 +0100

    removed unnecessary test

--- example-test/src/test/kotlin/TestSuite.kt
+++ example-test/src/test/kotlin/TestSuite.kt
@@ -1,12 +1,11 @@
+
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import tests.TextfieldsTests
-import tests.old.FirstTest
 
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-        FirstTest::class,
         TextfieldsTests::class
 )
 class TestSuite
