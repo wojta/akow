7849acf17fa10d84474fee27d41af21a4f0d9a78
Vojtěch Sázel <wojta@users.noreply.github.com>
2018-02-28 15:48:52 +0100

    Revert "Initial library version"

--- lib/src/main/kotlin/org/aerogear/akow/dsl/base/BaseNode.kt
+++
@@ -1,8 +0,0 @@
-package org.aerogear.akow.dsl.base
-
-/**
- * Base node for all Akow Apium configuration DSL.
- */
-abstract class BaseNode(override val nodeName: String) : HasParent {
-    internal abstract val children: MutableList<in Node>
-}
--- lib/src/main/kotlin/org/aerogear/akow/exceptions/CommandDuplicationException.kt
+++
@@ -1,6 +0,0 @@
-package org.aerogear.akow.exceptions
-
-/**
- * DSL section or command was added more than once where it's required to have only one.
- */
-class CommandDuplicationException(msg: String) : Exception(msg)
